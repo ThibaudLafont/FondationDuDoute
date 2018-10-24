fae1aae2512b4a945aa16363c43aa2d59a315086
ThibaudLafont <thibaudlafont@gmail.com>
2018-10-24 10:30:20 +0200

    Build homepage

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Book;
+use AppBundle\Entity\Post;
+use AppBundle\Entity\Website;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +14,31 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
+        // Get Entity Manager
+        $em = $this->getDoctrine()->getManager();
+
+        // Get number of books
+        $bookNbre = count($em->getRepository(Book::class)
+            ->findAll());
+        $bookNbre = $bookNbre >= 1 ? $bookNbre . ' livre' : $bookNbre . ' livres';
+
+        // Get number of posts
+        $postNbre = count($em->getRepository(Post::class)
+            ->findAll());
+        $postNbre = $postNbre >= 1 ? $postNbre . ' fiche' : $postNbre . ' fiches';
+
+        // Get number of websites
+        $websiteNbre = count($em->getRepository(Website::class)
+            ->findAll());
+        $websiteNbre = $websiteNbre >= 1 ? $websiteNbre . ' site' : $websiteNbre . ' sites';
+
         // replace this example code with whatever you need
-        return $this->render('default/homepage.html.twig');
+        return $this->render('default/homepage.html.twig', [
+            'bookNbre' => $bookNbre,
+            'postNbre' => $postNbre,
+            'websiteNbre' => $websiteNbre
+        ]);
     }
 }
